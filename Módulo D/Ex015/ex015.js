eae4abec42b67184fa6eb0717459d11ad3121d6e
Guilherme Lima <guilhermefk123@gmail.com>
2025-04-06 18:44:02 -0300

    atualizações no ex015

---
+++ Módulo D/Ex015/ex015.js
@@ -0,0 +1,12 @@
+function verificar() {
+    const data = new Date()
+    const ano = data.getFullYear()
+const fano = document.getElementById
+}
+
+// function carregarImagem {
+//     const msg = document.getElementById('msg')
+//     const img = document.getElementById('imagem')
+//     const idade
+// if(idade)
+// }
