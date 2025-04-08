8d2ef645695a7fbac80cdddcbca8372c9346cd3b
Guilherme Lima <guilhermefk123@gmail.com>
2025-04-07 21:58:25 -0300

    organizando pasta do ex015

--- Módulo D/Ex015/ex015.js
+++ Módulo D/Ex015/ex015.js
@@ -1,12 +1,13 @@
 function verificar() {
     const data = new Date()
     const ano = data.getFullYear()
-const fano = document.getElementById
+    const fano = document.getElementById('txtano')
+    const res document.querySelector('res')
+    if (fano.value.lenght == 0 || fano.value > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    }
+    else {
+const fsexo
+    }
 }
 
-// function carregarImagem {
-//     const msg = document.getElementById('msg')
-//     const img = document.getElementById('imagem')
-//     const idade
-// if(idade)
-// }
