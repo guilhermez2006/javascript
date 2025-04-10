d30146550dae38cc5783dddc45745ed1d9abe48c
Guilherme Lima <guilhermefk123@gmail.com>
2025-04-10 17:43:19 -0300

    Criando Ex016

---
+++ Módulo D/ambiente.js
@@ -0,0 +1,16 @@
+function RepeticaoFor(){
+for(let i = 0; i < 19; i++){
+    console.log("Usando for pra repetir essa frase 19 vezes")
+}
+}
+
+function RepeticaoWhile(){
+    let maçã = 1
+    while(maçã <= 15){
+        console.log("Maçã 15 vezes!")
+    maçã++
+}
+}
+RepeticaoWhile()
+
+
